0c4ca52f767b40eea292cac2c62b6096c3fb8bdf
roger9kuo <roger9kuo@berkeley.edu>
2018-10-07 15:32:57 -0700

    Update tickets.cpp
    - added some mutexes and conditions
    - added some seller initialization code

--- tickets.cpp
+++ tickets.cpp
@@ -17,6 +17,7 @@ using namespace std;
 #define NUM_THREADS 10
 
 int minute = 0; /* 0 - 59, in minutes */
+int availableSeats = 100; // 10 x 10 seats
 map<int, string> seats; /* map holding all the sold seats */
 // dynamically add seats as they're sold using key-value pairs of 
 // <seat #, customer name>
@@ -30,8 +31,26 @@ map<int, string> seats; /* map holding all the sold seats */
  */
 priority_queue<int> h1, m1, m2, m3, l1, l2, l3, l4, l5, l6;
 
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;		// Placeholder
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;	// Placeholder
+
+pthread_mutex_t seatsMutex = PTHREAD_MUTEX_INITIALIZER;	// Seats
+pthread_mutex_t buyersMutex = PTHREAD_MUTEX_INITIALIZER;// Buyers mutex - delete if we dont need
+
+pthread_cond_t HQcond = PTHREAD_COND_INITIALIZER;	// High Seller Queue condition
+pthread_mutex_t HQmutex = PTHREAD_MUTEX_INITIALIZER;	// High Seller Queue mutex
+pthread_cond_t MQcond = PTHREAD_COND_INITIALIZER;	// Mid Sellers Queue condition
+pthread_mutex_t MQmutex = PTHREAD_MUTEX_INITIALIZER;	// Mid Sellers Queue mutex
+pthread_cond_t LQcond = PTHREAD_COND_INITIALIZER;	// Low Sellers Queue condition
+pthread_mutex_t LQmutex = PTHREAD_MUTEX_INITIALIZER;	// Low Sellers Queue mutex
+
+bool soldOut()
+{
+    pthread_mutex_lock(seatsMutex);
+    bool result = (availableSeats == 0);
+    pthread_mutex_unlock(seatsMutex);
+    return result;
+}
 
 /* start routine which hanndles all transactions and printing 
  * where arg seller_type is either H, M or L
@@ -45,7 +64,38 @@ void * sell(char *seller_type) {
     // remember to give customer names based on seller and place in queue
     // remember to print 10x10 seating chart after each ticket sale
     // remember to update tracking vars/arrays
-    return NULL;
+	switch(seller_type) {	// Determine which mutex and condition to use
+        	case 'H': //high priority
+            		mutex = HQmutex;
+            		cond = HQcond;
+            		break;
+        	case 'M':  //medium priority
+            		mutex = MQmutex;
+            		cond = MQcond;
+            		break;
+        	case 'L':  //low priority
+            		waitMutex = LQmutex;
+            		waitCond = LQcond;
+            		break;
+    	}
+    	// while(true) {
+        //	pthread_mutex_lock(mutex);
+        //	pthread_cond_wait(cond, mutex);	// Block thread on condition variable
+        //	pthread_mutex_unlock(mutex);
+        //	if (soldOut()) { break;	}	// break if no more seats available
+        //	get next buyer			// code to get next buyer from queue
+        //	while (buyer != NULL)		// while buyer exists
+        //	{
+        //    		count++;		// increment sold count
+        //    		sell ticket		// code to sell ticket
+        //    		if(count!=10)		// while seller not sold out
+        //      		get next buyer	// code to get next buyer
+	//	}
+        //	if(soldOut()) { break; }	// break if no more seats available
+        //	else if(count==10){ break; }	// else break if seller is sold out
+    	// }
+    	// pthread_cancel(this thread ID);	// close this seller's thread
+    	return NULL;
 }
 
 /* function which wakes up threads from suspension to execute in parallel */
@@ -53,6 +103,15 @@ void wakeup_all_seller_threads() {
     // ROGER
     // dealing with mutex lock/unlock and cond broadcast stuff
     // sorry i can't help you much here :(
+	pthread_mutex_lock(HQmutex);	// High price seller queue
+    	pthread_cond_broadcast(HQcond);
+    	pthread_mutex_unlock(HQmutex);
+	pthread_mutex_lock(MQmutex);	// Mid price sellers queue
+    	pthread_cond_broadcast(MQcond);
+    	pthread_mutex_unlock(MQmutex);
+	pthread_mutex_lock(LQmutex);	// Low price sellers queue
+    	pthread_cond_broadcast(LQcond);
+    	pthread_mutex_unlock(LQmutex);
 }
 
 /* where arg N is the command line option for # of customers per queue */
